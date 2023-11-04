89afb67ef8b4156bf0e057997d6239f1a9ee0dbc
Andrew Stoyko <35904190+astoyko@users.noreply.github.com>
2023-11-04 12:44:55 -0500

    Comments and renaming

--- CubeStateDetection/vision/vision.py
+++ CubeStateDetection/vision/vision.py
@@ -29,20 +29,23 @@ BGRs = {
     5: (255, 255, 255)
 }
 
+# Paths
 basePath = "./CubeStateDetection/vision/"
 imagesPath = basePath + "images/"
 outPath = basePath + "output/"
 
+# Create output folder
 isExist = os.path.exists(outPath)
 if not isExist:
    os.makedirs(outPath)
 
+# Parameters
 cube = imagesPath + '/3x3 edge (4).jpg'
 edgeLength = 3
 
+# Read in the cube
 img = cv.imread(cube)
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-
 height = img.shape[0]
 width = img.shape[1]
 
@@ -69,30 +72,34 @@ colours[5] = white
 
 writeImages(colours)
 
+# Solve the cube
 solution = np.zeros((edgeLength, edgeLength))
 outImage = np.zeros((height, width, 3), dtype='uint8')
 
 for i in range(edgeLength):
     for j in range(edgeLength):
+
+        # Mask off one of the 9 colours on the cube face
         mask = np.zeros((height, width, 1), dtype='uint8')
         mask[(height//edgeLength) * i:(height//edgeLength) * (i+1), (width//edgeLength)*j:(width//edgeLength)*(j+1)] = 255
 
-        max = 0
+        # Find the colour that has the most pixels in that area
+        max =0
         id = 0
-
-        temp = 0
+        idNum = 0
         for colour in colours:
             masked = cv.bitwise_and(mask, colour)
             sum = np.sum(masked == 255)
             if(sum > max):
                 max = sum
-                id = temp
-            temp += 1
+                id = idNum
+            idNum += 1
 
-        cv.imwrite(outPath + 'blank.jpg', mask)
-
-        outImage[(height//edgeLength) * i:(height//edgeLength) * (i+1), (width//edgeLength)*j:(width//edgeLength)*(j+1)] = BGRs[id]
         solution[i][j] = id
+
+        # Output
+        cv.imwrite(outPath + 'blank.jpg', mask)
+        outImage[(height//edgeLength) * i:(height//edgeLength) * (i+1), (width//edgeLength)*j:(width//edgeLength)*(j+1)] = BGRs[id]
 
 print(solution)
 cv.imwrite(outPath + 'output.jpg', outImage)
