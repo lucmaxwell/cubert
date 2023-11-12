f7ca1ab8025732b23f91e8d1784e28a488024cb0
Duy Anh Nguyen <In_Drop@outlook.com>
2023-11-12 01:54:08 -0600

    Update Training.py

--- Training.py
+++ Training.py
@@ -1,21 +1,19 @@
 import math
 import os
 import time
-import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.monitor import Monitor
 import torch
+from stable_baselines3.common.monitor import Monitor
 
 from RubikCubeEnv import RubiksCubeEnv
+from Testing import testing
 
 # Hyperparameters
 NUM_PARALLEL_ENV = 10
 VERBOSE = 1
 
-TOTAL_TIME_STEPS = 1000000
-NUM_EPISODES = 1000
+TOTAL_TIME_STEPS = 100000
 
 NUM_SCRAMBLE = 1
 MAX_STEPS_ATTEMPT = int(math.ceil(NUM_SCRAMBLE * 2.5))
@@ -35,8 +33,8 @@ if __name__ == '__main__':
     env = RubiksCubeEnv()
 
     # Create a new model by default
-    model_file_path = os.path.join(save_path, MODEL_NAME)
-    if os.path.isfile(model_file_path + ".zip"):
+    model_file_path = os.path.join(save_path, MODEL_NAME + ".zip")
+    if os.path.isfile(model_file_path):
         print("Loading existing model...")
         training_model = PPO.load(model_file_path, env=env, verbose=VERBOSE, tensorboard_log=log_path, device="cuda")
     else:
@@ -46,27 +44,27 @@ if __name__ == '__main__':
     # Training
     total_steps = 0
     training_model.learn(total_timesteps=TOTAL_TIME_STEPS)
-    for episode in range(NUM_EPISODES):
+    while total_steps >= TOTAL_TIME_STEPS:
         env.scramble()
 
         solved = False
         num_steps = 0
         while not solved:
-            # Reset the Rubik's Cube
+            # Reset the Rubik's Cube to the scrambled state
             obs, _ = env.reset()
 
             while num_steps < MAX_STEPS_ATTEMPT:
                 # Action and reward
                 action, _ = training_model.predict(obs)
-                obs, reward, done, _, _ = env.step(action)
+                obs, reward, _, _, _ = env.step(action)
+
+                solved = env.cube.is_solved()
 
                 # Update the number of steps taken
                 num_steps += 1
 
-        # Done training
+        # Update the total number of steps
         total_steps += num_steps
-        if total_steps >= TOTAL_TIME_STEPS:
-            break
 
     # Save the model
     training_model.save(model_file_path)
@@ -80,7 +78,14 @@ if __name__ == '__main__':
     elapsed_time = end_time - start_time
     print(f"Elapsed time: {elapsed_time} seconds")
 
+    # Test
+    testing(MODEL_NAME)
+
     # Evaluate the model
-    eval_env = RubiksCubeEnv()
-    mean_reward, std_reward = evaluate_policy(training_model, eval_env, n_eval_episodes=10)
-    print(f"Evaluation: Mean reward: {mean_reward}, Std: {std_reward}")
+    num_scrambles = range(1, 13 + 1)
+    for num_scramble in num_scrambles:
+        eval_env = RubiksCubeEnv(num_scramble=num_scramble)
+        monitored_eval_env = Monitor(eval_env)
+
+        mean_reward, std_reward = evaluate_policy(training_model, monitored_eval_env, n_eval_episodes=100)
+        print(f"Evaluation {num_scramble}: Mean reward: {mean_reward}, Std: {std_reward}")
