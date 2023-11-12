7368352d77ed16d1d3419013265d195bb284a172
Duy Anh Nguyen <In_Drop@outlook.com>
2023-11-12 15:52:51 -0600

    Life is pretty good

--- TrainingNewAttempt.py
+++ TrainingNewAttempt.py
@@ -1,5 +1,6 @@
 import math
 import os
+import random
 import time
 from stable_baselines3 import PPO
 import torch
@@ -10,9 +11,7 @@ from Testing import testing
 # Hyperparameters
 VERBOSE = 0
 
-
 MODEL_NAME = "ppo_episode_train"
-
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -35,41 +34,58 @@ if __name__ == '__main__':
         training_model = PPO('MlpPolicy', env=env, verbose=VERBOSE, device="cuda")
 
     # Training
-    num_scramble_list = range(3, 3 + 1)
-    for num_scramble in num_scramble_list:
-        print(f"Number of scrambles = {num_scramble}")
-        env.set_num_scramble(num_scramble)
+    TOTAL_EPISODE = 100
+    for episode in range(TOTAL_EPISODE):
+        # Random number of scramble
+        num_scramble_list = range(1, 2 + 1)
+        num_scramble = random.choice(num_scramble_list)
+        print(f"Episode: {episode + 1}, number of scramble: {num_scramble}")
 
-        TOTAL_EPISODE = 100
+        # Setup to run the episode
         MAX_STEPS_PER_EPISODE = int(math.ceil(num_scramble * 2.5))
-        MAX_STEPS = TOTAL_EPISODE * MAX_STEPS_PER_EPISODE
+        env.set_num_scramble(num_scramble)
+        episode_reward = 0
+        num_steps_to_solved = 0
+        training_model.ent_coef = 0.0
 
-        for episode in range(TOTAL_EPISODE):
-            episode_reward = 0
-            num_steps_to_solved = 0
+        # New scramble cube
+        env.scramble()
 
-            # New scramble cube
-            env.scramble()
+        # Solve the Rubik's Cube
+        attempt = 0
+        solved = False
+        while not solved:
+            attempt += 1
 
-            solved = False
-            while not solved:
-                obs, _ = env.reset()
+            obs, _ = env.reset()
 
-                for step in range(MAX_STEPS_PER_EPISODE):
-                    # Action and reward
-                    action, _ = training_model.predict(obs)
-                    obs, reward, solved, _, _ = env.step(action)
+            for step in range(MAX_STEPS_PER_EPISODE):
+                # Action and reward
+                action, _ = training_model.predict(obs)
+                obs, reward, solved, _, _ = env.step(action)
 
-                    episode_reward += reward
+                episode_reward += reward
 
-                    if solved:
-                        num_steps_to_solved = step + 1
-                        break
+                if solved:
+                    num_steps_to_solved = step + 1
+                    break
 
-            training_model.learn(total_timesteps=MAX_STEPS_PER_EPISODE)
+            # If not solved, increase the exploration
+            if not solved and attempt % 10 == 0:
+                if training_model.ent_coef < 1.0:
+                    training_model.ent_coef += 0.01
 
-            # Print episode information
-            print(f"Episode: {episode + 1}, Number of steps solved: {num_steps_to_solved + 1}, Total Reward: {episode_reward}")
+            # Learn
+            if solved:
+                training_model.learn(total_timesteps=num_steps_to_solved)
+
+                # There is a better solution
+                if num_steps_to_solved > num_scramble:
+                    print(f"Not optimal solution. Number of steps solved: {num_steps_to_solved}, ent_coef: {training_model.ent_coef}")
+                    solved = False
+
+        # Print episode information
+        print(f"Number of steps solved: {num_steps_to_solved}, Total Reward: {episode_reward}")
 
     # Save the model
     training_model.save(model_file_path)
