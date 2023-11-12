b2da3a244a8331112c7cb44de3099dc1ebe05c3a
Duy Anh Nguyen <In_Drop@outlook.com>
2023-11-11 18:41:31 -0600

    Update RubikCubeEnv.py
    Change the environment based on the new implementation of the Rubik Cube model.

--- RubikCubeEnv.py
+++ RubikCubeEnv.py
@@ -4,39 +4,32 @@ from stable_baselines3.common.env_checker import check_env
 
 from RubikCube import RubikCube, Face, SquareColour
 
-# Global cube size
-CUBE_SIZE = 3
-
 # Define the action space dictionary globally
 TOTAL_FACES = 6
-TOTAL_SPINS = 4  # Left, right, down, up
-TOTAL_ACTIONS = TOTAL_FACES * TOTAL_SPINS * CUBE_SIZE  # Each face has the cube size selection of row or column
-
-NUM_SCRAMBLE = 1
-
-TOTAL_SQUARES = CUBE_SIZE ** 2 * TOTAL_FACES
-
+TOTAL_SPINS = 2  # Clockwise, Counter-clockwise
 
 def decode_action(action):
-    face = action // (TOTAL_SPINS * CUBE_SIZE)
-    remaining = action % (TOTAL_SPINS * CUBE_SIZE)
-    spin = remaining // CUBE_SIZE
-    row_or_col = remaining % CUBE_SIZE
-    return face, spin, row_or_col
+    face = action // TOTAL_SPINS
+    spin = action % TOTAL_SPINS
+    return face, spin
 
 
 class RubiksCubeEnv(gymnasium.Env):
-    def __init__(self):
+    def __init__(self, num_scramble=1, cube_size=3):
         super(RubiksCubeEnv, self).__init__()
 
+        self.cube_size = cube_size
+
         # Define action and observation space
-        self.action_space = gymnasium.spaces.Discrete(TOTAL_ACTIONS)
+        self.action_space = gymnasium.spaces.Discrete(TOTAL_FACES * TOTAL_SPINS)
         self.observation_space = gymnasium.spaces.Box(
             low=0, high=(TOTAL_FACES - 1),
-            shape=(TOTAL_FACES, CUBE_SIZE, CUBE_SIZE),
+            shape=(TOTAL_FACES, cube_size, cube_size),
             dtype=np.uint8)
 
-        self.cube = RubikCube(CUBE_SIZE)
+        self.num_scramble = num_scramble
+
+        self.cube = RubikCube(self.cube_size)
         self.initial_cube_obs = self._get_observation()
         self.scramble()
 
@@ -44,8 +37,8 @@ class RubiksCubeEnv(gymnasium.Env):
         self.reward_range = (-1, 1)
 
     def scramble(self):
-        self.cube = RubikCube(CUBE_SIZE)
-        self.cube.scramble(NUM_SCRAMBLE)
+        self.cube = RubikCube(self.cube_size)
+        self.cube.scramble(self.num_scramble)
         self.initial_cube_obs = self._get_observation()
 
     def reset(self, **kwargs):
@@ -54,37 +47,28 @@ class RubiksCubeEnv(gymnasium.Env):
 
     def step(self, action):
         # Decode the action
-        face, spin, row_or_col = decode_action(action)
-
-        # print("Initial state:")
-        # self.cube.print_cube_state()
-        # print(f"Action face {face} spin {spin} row/col {row_or_col}")
+        face, spin = decode_action(action)
+        face = Face(face)
 
         # Apply the action
         if spin == 0:
-            self.cube.rotate_row_left(face, row_or_col)
+            self.cube.rotate_clockwise(face)
         elif spin == 1:
-            self.cube.rotate_row_right(face, row_or_col)
-        elif spin == 2:
-            self.cube.rotate_column_down(face, row_or_col)
-        elif spin == 3:
-            self.cube.rotate_column_up(face, row_or_col)
+            self.cube.rotate_counter_clockwise(face)
 
         # Calculate reward based on the number of correct squares
         done = self.cube.is_solved()
         reward = 1 if done else -1
 
-        # print("Action result:")
-        # self.cube.print_cube_state()
-
         # Return
         return self._get_observation(), reward, done, False, {}
 
     def render(self):
-        self.cube.print_cube_state()
+        #self.cube.print_cube_state()
+        pass
 
     def _get_observation(self):
-        observation = np.zeros((TOTAL_FACES, CUBE_SIZE, CUBE_SIZE), dtype=np.uint8)
+        observation = np.zeros((TOTAL_FACES, self.cube_size, self.cube_size), dtype=np.uint8)
         for i, face in enumerate(Face):
             observation[i, :, :] = self.cube.get_face_colors(face)
 
