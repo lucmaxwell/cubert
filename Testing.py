8d39f1028c9706b632f0544d7fea994b2c0b7b89
Duy Anh Nguyen <In_Drop@outlook.com>
2023-11-14 00:49:16 -0600

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -6,43 +6,63 @@ from matplotlib import pyplot as plt
 from stable_baselines3 import PPO
 
 from RubikCubeEnv import RubiksCubeEnv
+from UtilityFunctions import load_model_PPO, load_model_DQN
 
-MODEL_NAME = "ppo_episode_train"
+
+def validate_reinforcement(model, env, num_episodes=10):
+    success_count = 0
+    optimal_success_count = 0
+    total_steps = 0
+
+    for episode in range(num_episodes):
+        # New scramble cube
+        env.scramble()
+
+        obs, _ = env.reset()
+
+        solved = False
+        steps = 0
+        while not solved and steps < env.get_max_steps_per_episode():
+            steps += 1
+
+            action, _ = model.predict(obs, deterministic=True)
+            obs, _, solved, _ = env.step(action)
+
+        if solved:
+            success_count += 1
+
+            # Optimally solved
+            if steps <= env.get_num_scramble():
+                optimal_success_count += 1
+
+        total_steps += steps
+
+    average_steps = total_steps / num_episodes
+    success_rate = success_count / num_episodes
+    optimal_success_rate = optimal_success_count / num_episodes
+
+    # Return
+    return average_steps, success_rate, optimal_success_rate
 
 
-def testing(model_name):
-    start_time = time.time()
-
-    save_path = os.path.join('Training', 'Saved Models')
-
-    # Create the environment and vector for parallel environments
-    env = RubiksCubeEnv()
-
-    # Load the model
-    model_file_path = os.path.join(save_path, model_name + ".zip")
-    if not os.path.isfile(model_file_path):
-        raise FileNotFoundError(f"Model file not found: {model_file_path}")
-    print("Loading existing model...")
-    training_model = PPO.load(model_file_path, env=env)
-
+def test(model, env, plot_title):
     # Test the trained agent
     solved_count_list = []
     num_scrambles = range(1, 13 + 1)
     for num_scramble in num_scrambles:
-        MAX_STEPS = int(math.ceil(num_scramble * 2.5))
         env.set_num_scramble(num_scramble)
+        MAX_STEPS = env.get_max_steps()
 
         solved_count = 0
         for _ in range(100):
-            env.scramble()
-            obs, _ = env.reset()
+            obs = env.scramble()
 
             # Solve
             solved = False
             num_steps = 0
             while not solved and num_steps < MAX_STEPS:
                 # Action and reward
-                action, _ = training_model.predict(obs, deterministic=True)
+                action, _ = model.predict(obs, deterministic=True)
                 obs, reward, solved, _, _ = env.step(action)
 
                 num_steps += 1
@@ -54,15 +74,10 @@ def testing(model_name):
 
         print(f"Scramble {num_scramble:<2d} {solved_count}%")
 
-    # End time
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Elapsed time: {elapsed_time} seconds")
-
     # Create the plot
     plt.figure(figsize=(10, 6))
     plt.plot(num_scrambles, solved_count_list, marker='o')
-    plt.title('Solved Counts vs Number of Scrambles')
+    plt.title(plot_title)
     plt.xlabel('Number of Scrambles')
     plt.ylabel('Solved Counts')
     plt.xticks(num_scrambles)
@@ -78,4 +93,14 @@ def testing(model_name):
 
 
 if __name__ == '__main__':
-    testing(MODEL_NAME)
+    MODEL_NAME = "dqn_train_100"
+
+    save_path = os.path.join('Training', 'Saved Models')
+
+    # Create the environment and vector for parallel environments
+    environment = RubiksCubeEnv()
+
+    # Create a new model by default
+    test_model = load_model_DQN(save_path, MODEL_NAME, environment)
+
+    test(test_model, environment, MODEL_NAME)
