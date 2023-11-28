cc12187945959b98a95039bf68b09e415f2a867c
Andrew Stoyko <35904190+astoyko@users.noreply.github.com>
2023-11-28 17:11:35 -0600

    Image capturing script, lots of images

---
+++ CubeStateDetection/vision/image.py
@@ -0,0 +1,40 @@
+import cv2 as cv
+import numpy as np
+import os
+import urllib.request
+
+def getImage(url):
+    req = urllib.request.urlopen(url)
+    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    img = cv.imdecode(arr, -1) # 'Load it as it is'
+    return img
+
+print(f'Working in {os.getcwd()}')
+
+# Parameters
+basePath = os.getcwd() + "\\CubeStateDetection\\vision\\"
+imagesPath = basePath + "images/"
+imageUrl = "http://192.168.4.1/capture"
+
+# Create output folder
+isExist = os.path.exists(imagesPath)
+if not isExist:
+   os.makedirs(imagesPath)
+
+img = getImage(imageUrl)
+
+top = 162
+left = 249
+height = 324
+width = height
+
+img = img[top:top+height, left:left+width, :]
+
+number = 0
+image = f"angle {number}.jpg"
+
+while(os.path.exists(imagesPath + image)):
+    number += 1
+    image = f"angle {number}.jpg"
+
+cv.imwrite(imagesPath + image, img)
