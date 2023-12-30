b92e77a47d2b53c2078914afb25b67f0d8a3f554
Duy Anh Nguyen <In_Drop@outlook.com>
2023-12-29 18:39:44 -0600

    Update DQN_Ascending.py

--- DQN_Ascending.py
+++ DQN_Ascending.py
@@ -5,62 +5,111 @@ import numpy as np
 import torch
 from stable_baselines3 import DQN
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
-from stable_baselines3.common.vec_env import SubprocVecEnv, DummyVecEnv
 from torch import nn
 
-from Network import AscendingNetwork
 from RubikCubeEnv import RubiksCubeEnv
 from Model_Validation import evaluate_model, test
 
+TOTAL_STEPS = 100_000
+MODEL_NAME = "dqn_ascending"
 
-# class AscendingNetwork(BaseFeaturesExtractor):
-#     def __init__(self, input_obs_space, features_dim, hidden_size=512):
-#         super(AscendingNetwork, self).__init__(input_obs_space, features_dim)
-#
-#         # Dynamically calculate the flattened size of the observation space
-#         flattened_obs_space = int(np.prod(input_obs_space.shape))
-#
-#         # Define the network layers
-#         self.network = nn.Sequential(
-#             nn.Linear(flattened_obs_space, hidden_size),
-#             nn.LeakyReLU(),
-#
-#             nn.Linear(hidden_size, hidden_size * 2),
-#             nn.LeakyReLU(),
-#             nn.BatchNorm1d(hidden_size * 2),
-#
-#             nn.Linear(hidden_size * 2, hidden_size * 3),
-#             nn.LeakyReLU(),
-#             nn.BatchNorm1d(hidden_size * 3),
-#
-#             nn.Linear(hidden_size * 3, hidden_size * 2),
-#             nn.LeakyReLU(),
-#
-#             nn.Linear(hidden_size * 2, 256),
-#             nn.LeakyReLU(),
-#
-#             nn.Linear(256, features_dim)
-#         )
-#
-#     def forward(self, observations):
-#         # Flatten the observations
-#         obs_flat = torch.flatten(observations, start_dim=1)
-#
-#         return self.network(obs_flat)
+NUM_SCRAMBLES = 3
+
+
+class AscendingNetwork(BaseFeaturesExtractor):
+    def __init__(self, input_obs_space, features_dim, hidden_size=2048):
+        super(AscendingNetwork, self).__init__(input_obs_space, features_dim)
+
+        # Define the convolutional network layers
+        TOTAL_FACES = 6
+        last_conv_layer_dim = 128
+        self.conv_layers = nn.Sequential(
+            # Convolutional Layer 1: Input is the 6 faces of the cube
+            nn.Conv2d(in_channels=TOTAL_FACES, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            # Convolutional Layer 2
+            nn.Conv2d(in_channels=64, out_channels=last_conv_layer_dim, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(last_conv_layer_dim),
+            nn.ReLU(),
+        )
+
+        # Define the network layers
+        cube_size = input_obs_space.shape[1]
+        conv_output_size = self.calculate_conv_output_size(cube_size, 3, 1, 1)
+        conv_output_size = self.calculate_conv_output_size(conv_output_size, 3, 1, 1)
+        flattened_conv = conv_output_size * conv_output_size * last_conv_layer_dim
+
+        self.lstm = nn.LSTM(input_size=flattened_conv, hidden_size=hidden_size, batch_first=True)
+
+        self.residual_block = ResidualBlock(hidden_size)
+
+        self.fc = nn.Linear(hidden_size, features_dim)
+
+        # self.network = nn.Sequential(
+        #
+        #     nn.LSTM(input_size=flattened_conv, hidden_size=hidden_size, batch_first=True),
+        #
+        #     ResidualBlock(hidden_size),
+        #
+        #     # Output layer
+        #     nn.Linear(hidden_size, features_dim)
+        # )
+
+    def calculate_conv_output_size(self, input_size, kernel_size, stride, padding):
+        return ((input_size + 2 * padding - kernel_size) // stride) + 1
+
+    def forward(self, observations):
+        # Convolution layer input
+        conv_out = self.conv_layers(observations)
+
+        # Flatten the convolutional output
+        batch_size = conv_out.size(0)
+        conv_out_flat = conv_out.view(batch_size, -1)
+
+        # Reshape for LSTM - treat the entire set of features from convolutional layers as one sequence
+        lstm_in = conv_out_flat.unsqueeze(1)
+
+        # LSTM layer input
+        lstm_out, _ = self.lstm(lstm_in)
+        # Take the output for the last time step
+        lstm_out = lstm_out[:, -1, :]
+
+        # Apply residual block on LSTM output
+        res_out = self.residual_block(lstm_out)
+
+        # Fully connected layer input
+        output = self.fc(res_out)
+        return output
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, hidden_size):
+        super(ResidualBlock, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.LeakyReLU(),
+            nn.Dropout(0.1),
+
+            nn.Linear(hidden_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.LeakyReLU(),
+            nn.Dropout(0.1),
+        )
+
+    def forward(self, x):
+        return x + self.fc(x)  # Add input to the output (residual connection)
 
 
 def make_env(num_scrambles):
     def _init():
         env = RubiksCubeEnv(num_scramble=num_scrambles)
         return env
+
     return _init
 
-
-NUM_ENVS = 4
-TOTAL_STEPS = 400_000
-MODEL_NAME = f"dqn_ascending_400000"
-
-NUM_SCRAMBLES = 1
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -107,7 +156,7 @@ if __name__ == '__main__':
 
     # Evaluate
     start_time = time.time()
-    #evaluate_model(training_model, MODEL_NAME)
+    # evaluate_model(training_model, MODEL_NAME)
     test(training_model, MODEL_NAME)
     end_time = time.time()
     elapsed_time = end_time - start_time
