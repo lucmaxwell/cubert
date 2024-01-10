340226c83f4cb4ca0c33b8e8bfeecfc5f8f99c92
Andrew Stoyko <35904190+astoyko@users.noreply.github.com>
2024-01-10 09:47:16 -0600

    First working bluetooth connection

--- CubeStateDetection/bluetooth-test.py
+++ CubeStateDetection/bluetooth-test.py
@@ -1,13 +1,15 @@
 import socket
+import time
 
 client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-client.connect(("40:22:D8:F0:E6:18", 2))
+client.connect(("40:22:D8:F0:E6:1A", 1))
 
 print("Connected?")
  
 while True:
-    client.send("Reverse testing")
-    data = client.recv(4)
+    client.send("Reverse testing\r\n".encode("utf-8"))
+    data = client.recv(9)
     if not data:
         break
     print(f"Received: {data}")
+    time.sleep(1)
