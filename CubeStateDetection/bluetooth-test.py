84f0c4741cba6047cba9050dd7660fa2c1e87f8c
Andrew Stoyko <35904190+astoyko@users.noreply.github.com>
2024-01-11 23:08:13 -0600

    Sends command after receiving ack from ESP32

--- CubeStateDetection/bluetooth-test.py
+++ CubeStateDetection/bluetooth-test.py
@@ -1,26 +1,44 @@
 import socket
-import time
+import msvcrt
+import random
 
-y = 0
-y_ = 1
-b = 10
-b_ = 10
-x_ = 20
-req = 69
-ack = 999
+y = b'y'
+y_ = b'Y'
+b = b'b'
+b_ = b'B'
+x_ = b'X'
+ack = b'a'
 
-test = [y, y_, b, b_, x_]
+messages = [y, y_, b, b_, x_]
 
 client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 client.connect(("40:22:D8:F0:E6:1A", 1))
+client.setblocking(False)
 
 print("Connected?")
+message = ""
 
 while True:
-    data = client.recv(9)
-    if not data:
-        print(f"No message received, doing nothing")
-        time.sleep(0.1)
-        break
+    if msvcrt.kbhit():
+        letter = msvcrt.getch().decode("utf-8")
+        message += letter
+        
+        if letter == "\r":
+            client.send(message.encode("utf-8"))
+            message = ""
 
-    print(f"Received: {data}")
+            print()
+        print(letter, end="")
+
+    try:
+        data = client.recv(4)
+        print(f"Received: {data}")
+
+        if data == ack:
+            msg = random.choice(messages)
+            print(f"Sending: {msg}")
+            client.send(msg)
+            client.send(b'\r')
+
+    except:
+        print("",end="")
