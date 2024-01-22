99477f9b6feffc0a7f059ffd50210cc6d1697d0f
Andrew Stoyko <35904190+astoyko@users.noreply.github.com>
2024-01-21 21:38:22 -0600

    Created a script to process all cubes in the images/cubes folder

---
+++ CubeStateDetection/vision/process-cubes.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import numpy as np
+import os
+from sklearn.cluster import KMeans
+import scipy.stats as stats
+import vision
+
+basePath = os.getcwd() + "\\CubeStateDetection\\vision\\"
+cubeFolder = basePath + "\images\cubes\\"
+imagesPath = basePath + "images/"
+mask = 'mask3.png'
+maskPath = imagesPath + mask
+
+num = 0
+name = f"cube{num}.png"
+
+while(os.path.exists(cubeFolder + name)):
+    num += 1
+    name = f"cube{num}.png"
+
+combinedMask = vision.loadMask(maskPath)
+
+for i in range(num):
+    cubePath = cubeFolder + f"cube{i}.png"
+    cube = vision.loadCube(cubePath)
+    # autoMask = vision.getAutoMask(cube, 75, 512)
+    # combinedMask = mask * autoMask
+    
+
+    solution, outImage = vision.getCubeState(cube, combinedMask, 3, 18, True)
+    cv.imwrite(cubeFolder + f"solution{i}.png", outImage)
+    print(f"{i}: done")
