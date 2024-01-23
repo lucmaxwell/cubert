42a2fc70d6e60969a826fd9425d952c816069d73
Duy Anh Nguyen <In_Drop@outlook.com>
2024-01-23 16:47:09 -0600

    Update CubertArm.py

--- CubertArm.py
+++ CubertArm.py
@@ -8,6 +8,7 @@ from Motor import CubertMotor, MotorSpin
 
 
 # Parameters
+NUM_STEPS_CALIBRATION = 20
 NUM_STEPS_FROM_TOP_TO_MIDDLE = 1200
 NUM_STEPS_FROM_TOP_TO_DROP_OFF = 600
 NUM_STEPS_FROM_DROP_OFF_TO_MIDDLE = 700
@@ -42,13 +43,35 @@ class CubertArm:
         self.end_stop_arm_lower_limit = CubertButton(end_stop_arm_pin_list[1], GPIO.LOW)
         self.end_stop_hand_open_limit = CubertButton(end_stop_arm_pin_list[2], GPIO.LOW)
 
-        # Ensure that the hand is open
-        self.open_hand(60)
+        # # Ensure that the hand is open
+        # self.open_hand(60)
+        #
+        # # Arm calibration
+        # self.arm_calibration = 0
+        # while not self.end_stop_arm_lower_limit.pressed():
+        #     self.move_arm_to_bottom()
+        #
+        #
+        #
+        #
+        # self.arm_position = ArmPosition.MIDDLE
+        # self.move_arm_to_bottom(60)
+        # while not self.end_stop_arm_upper_limit.pressed():
+        #     self.move_arm(ArmDirection.UP, 60)
+        #     self.arm_calibration += 1
+        #
+        # # Move the arm to default middle to position to indicate calibration completed
+        # self.move_arm_to(ArmPosition.MIDDLE, 60)
 
-        # Arm default position
-        self.arm_position = ArmPosition.TOP
-        self.move_arm_to(ArmPosition.TOP, 60)
-        self.move_arm_to(ArmPosition.MIDDLE, 60)
+    def calibrate(self, arm_position, move_speed):
+        move_direction = ArmDirection.DOWN
+        if arm_position == ArmPosition.BOTTOM:
+            self.move_arm(ArmDirection.UP, move_speed)
+
+        # Calibrate
+        if arm_position == ArmPosition.TOP or arm_position == ArmPosition.BOTTOM:
+            for _ in range(NUM_STEPS_CALIBRATION):
+                self.move_arm(move_direction, move_speed)
 
     def move_arm(self, arm_direction, move_speed):
         # End stop is used to determine if the motor can move the arm
@@ -77,21 +100,21 @@ class CubertArm:
             self.left_motor.step(1, MotorSpin.COUNTER_CLOCKWISE, move_speed)
             self.right_motor.step(1, MotorSpin.COUNTER_CLOCKWISE, move_speed)
 
-    def move_to_top(self, move_speed):
+    def move_arm_to_top(self, move_speed):
         self.arm_position = ArmPosition.TOP
         while not self.end_stop_arm_upper_limit.pressed():
             self.move_arm(ArmDirection.UP, move_speed)
 
-    def move_to_bottom(self, move_speed):
+    def move_arm_to_bottom(self, move_speed):
         self.arm_position = ArmPosition.BOTTOM
         while not self.end_stop_arm_lower_limit.pressed():
             self.move_arm(ArmDirection.DOWN, move_speed)
 
     def move_arm_to(self, position, move_speed):
         if position == ArmPosition.TOP:
-            self.move_to_top(move_speed)
+            self.move_arm_to_top(move_speed)
         elif position == ArmPosition.BOTTOM:
-            self.move_to_bottom(move_speed)
+            self.move_arm_to_bottom(move_speed)
         elif position == ArmPosition.MIDDLE:
             # From top
             if self.arm_position == ArmPosition.TOP:
