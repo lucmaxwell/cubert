b3564c7549f2c3c1a2ba57cc19f89a998aec9802
moram03 <mamora0312@gmail.com>
2024-01-26 11:44:26 -0600

    More Setup Changes

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ apt update && upgrade -y
 apt install python3-venv libopenblas-dev -y
 mkdir cubertEnv
 python3 -m venv cubertEnv
-source cubertEnv/bin/activate
+source ./cubertEnv/bin/activate
 pip install -r requirements.txt
 pip list
