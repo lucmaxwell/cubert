7ad4830c45a9843fbff45db87845e36c548e2b44
moram03 <mamora0312@gmail.com>
2024-01-26 13:57:04 -0600

    Removed old setup file

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update && upgrade -y
-sudo apt install python3-venv libopenblas-dev -y
-mkdir cubertEnv
-python3 -m venv cubertEnv
-source ./cubertEnv/bin/activate
-pip install -r requirements.txt
-pip list
