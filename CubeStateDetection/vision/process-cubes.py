a7b0c2c464971431903beb27e9fc8ce3ab1b45d6
Andrew Stoyko <35904190+astoyko@users.noreply.github.com>
2024-01-26 15:17:30 -0600

    Can now change size of images at process time

--- CubeStateDetection/vision/process-cubes.py
+++ CubeStateDetection/vision/process-cubes.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import os
+import sys
 from sklearn.cluster import KMeans
 import scipy.stats as stats
 import vision
@@ -14,15 +15,23 @@ maskPath = imagesPath + mask
 num = 0
 name = f"cube{num}.png"
 
+height = 339
+if(len(sys.argv) > 1):
+    height = int(sys.argv[1])
+
+length = height * 6
+
 while(os.path.exists(cubeFolder + name)):
     num += 1
     name = f"cube{num}.png"
 
 mask = vision.loadMask(maskPath)
+mask = cv.resize(mask, (length, height))
 
 for i in range(num):
     cubePath = cubeFolder + f"cube{i}.png"
     cube = vision.loadCube(cubePath)
+    cube = cv.resize(cube, (length, height))
     # autoMask = vision.getAutoMask(cube, 75, 512)
     # combinedMask = mask * autoMask
     combinedMask = mask
