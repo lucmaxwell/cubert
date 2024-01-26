c85db6371ea061d8f5e7794cfe01ceb8447e8cee
moram03 <mamora0312@gmail.com>
2024-01-26 11:28:12 -0600

    Setup Script
    Added a script to easily setup any Raspberry Pi

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update && upgrade -y
+apt install python3-venv libopenblas-dev -y
+mkdir cubertEnv
+python3 -m venv cubertEnv
+source cubertEnv/bin/activate
+pip install -r requiments.txt
