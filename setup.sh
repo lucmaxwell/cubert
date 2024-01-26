1cf91f5bae557f4237e0096bf3318d75c0614a91
moram03 <mamora0312@gmail.com>
2024-01-26 11:34:11 -0600

    Fixed typo in setup script

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ apt install python3-venv libopenblas-dev -y
 mkdir cubertEnv
 python3 -m venv cubertEnv
 source cubertEnv/bin/activate
-pip install -r requiments.txt
+pip install -r requirements.txt
+pip list
