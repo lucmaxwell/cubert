9aeea73e4ec16165a8fa0e02766c8f07b5e540a4
moram03 <mamora0312@gmail.com>
2024-02-10 09:08:22 -0600

    Added Zen Mode

--- Actions.py
+++ Actions.py
@@ -1,5 +1,16 @@
 import Motor
 import time
+import signal
+import sys
+import RPi.GPIO as GPIO
+import random
+
+def sigint_handler(sig, frame):
+    del actions
+    del motor
+
+    GPIO.cleanup()
+    sys.exit(0)
 
 class CubertActions:
 
@@ -25,6 +36,29 @@ class CubertActions:
     def rotateCube(self, rotation:Motor.BaseRotation, direction:Motor.Direction):
         self.motor.spinBase(rotation, direction)
 
+    def zen(self):
+        while True:
+            move = random.randint(1,9)
+
+            if move == 1:
+                self.flip()
+            elif move == 2:
+                self.rotateCube(Motor.BaseRotation.QUARTER, Motor.Direction.CCW)
+            elif move == 3:
+                self.rotateCube(Motor.BaseRotation.QUARTER, Motor.Direction.CW)
+            elif move == 4:
+                self.rotateCube(Motor.BaseRotation.HALF, Motor.Direction.CCW)
+            elif move == 5:
+                self.rotateCube(Motor.BaseRotation.HALF, Motor.Direction.CW)
+            elif move == 6:
+                self.rotateFace(Motor.BaseRotation.QUARTER, Motor.Direction.CCW)
+            elif move == 7:
+                self.rotateFace(Motor.BaseRotation.QUARTER, Motor.Direction.CW)
+            elif move == 8:
+                self.rotateFace(Motor.BaseRotation.HALF, Motor.Direction.CCW)
+            elif move == 9:
+                self.rotateFace(Motor.BaseRotation.HALF, Motor.Direction.CW)
+
     
 
 if __name__ == '__main__':
@@ -36,6 +70,8 @@ if __name__ == '__main__':
     end_stop_hand_open_pin      = 16  # GPIO number for arm open limit end stop
     end_stop_arm_upperLimit_pin = 20  # GPIO number for arm upper limit end stop
     end_stop_arm_lowerLimit_pin = 21  # GPIO number for arm lower limit end stop
+
+    signal.signal(signal.SIGINT, sigint_handler)
 
     # initialize motor
     motor = Motor.CubertMotor(motor_en_pin, motor_step_pin, motor_dir_pin, end_stop_arm_upperLimit_pin, end_stop_arm_lowerLimit_pin, end_stop_hand_open_pin)
@@ -54,7 +90,11 @@ if __name__ == '__main__':
     time.sleep(1)
     actions.rotateFace(Motor.BaseRotation.HALF, Motor.Direction.CCW)
 
+    actions.zen()
+
 
     del actions
     del motor
+
+    GPIO.cleanup()
 
