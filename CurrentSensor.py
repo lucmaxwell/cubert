19b1faed9ffbc79e04c14280aca5e51d3e8a7ef7
moram03 <mamora0312@gmail.com>
2024-02-11 16:29:51 -0600

    Current Sensor Measuring Test

--- CurrentSensor.py
+++ CurrentSensor.py
@@ -1,5 +1,25 @@
 import time
+from enum import IntEnum
 import INA3221.SDL_Pi_INA3221 as INA3221
+
+class CurrentChannel(IntEnum):
+    LEFT_MOTOR  = 1
+    RIGHT_MOTOR = 2
+    BASE_LIGHT  = 3
+
+class CurrentSensor():
+
+    def __init__(self):
+        self.sensor = INA3221.SDL_Pi_INA3221(addr=0x40)
+
+    def getChannelCurrent(self, channel:CurrentChannel):
+        return self.sensor.getCurrent_mA(channel)
+    
 
 if __name__ == '__main__':
     print("Running Current Sensor Test")
+
+    sensor = CurrentSensor()
+
+    while True:
+        print("Base Light Current is %fmA" % sensor.getChannelCurrent(CurrentChannel.BASE_LIGHT))
