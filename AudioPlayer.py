97f6d74b740d6f0a46d9cd8225ca1769bdeeb404
moram03 <mamora0312@gmail.com>
2024-02-11 19:40:11 -0600

    Audio Player

---
+++ AudioPlayer.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+
+
+
+class CubertAudioPlayer():
+
+    def __init__(self, pwm_pin):
+
+        # setup GPIO
+        GPIO.setmode(GPIO.BCM)
+
+        # save pin
+        self._pwm_pin = pwm_pin
+
+        # setup pin
+        GPIO.PWM(pwm_pin)
+
+    def __del__(self):
+        GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    audio_pin = 12
+
+    audio = CubertAudioPlayer(audio_pin)
