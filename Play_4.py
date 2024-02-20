e554e6bf75dda34f0bed58dd19d4a1a2d324ae2f
moram03 <mamora0312@gmail.com>
2024-02-20 13:41:27 -0600

    PI Port

--- Play_4.py
+++ Play_4.py
@@ -5,8 +5,13 @@ import numpy as np
 import imutils
 import cv2
 import math
+import Vision
 
-img = cv2.imread(r'./images.jpg')
+
+vision = Vision.CubertVision()
+Vision.CubertVision.capture()
+
+img = cv2.imread(r'./image.jpg')
 # Image Checking
 if img is None:
   print("Error: File not found")
@@ -91,7 +96,4 @@ dist = math.sqrt(a+b)
 #print(pixel_per_cm)
 print(dist/pixel_per_cm)
 
-cv2.imshow('Input Image', output)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
