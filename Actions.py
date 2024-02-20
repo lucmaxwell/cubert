d86a9fe7832f894b20eec2668f012379c35bac20
moram03 <mamora0312@gmail.com>
2024-02-20 12:54:13 -0600

    Speed Up

--- Actions.py
+++ Actions.py
@@ -27,9 +27,9 @@ class CubertNotation(IntEnum):
 
 class CubertActions:
 
-    _default_base_speed = 250
+    _default_base_speed = 300
     _base_accel_frac    = 0.05      # Base Max Speed Point 
-    _default_arm_speed  = 120
+    _default_arm_speed  = 150
     _arm_accel_frac     = 0.15      # Arm Max Speed Point
     _cube_face_spun     = False     # tracks if cube state was spun recently
 
