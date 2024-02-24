4a3b25f2171a26ded7951740e163b7f1e278eb39
Andrew Stoyko <35904190+astoyko@users.noreply.github.com>
2024-02-24 17:21:24 -0600

    Lowered Skip Threshold

--- CurrentSensor.py
+++ CurrentSensor.py
@@ -17,7 +17,7 @@ class CubertCurrentSensor():
 
     run_gripper_monitor = threading.Event()
     
-    _current_threshold = 100
+    _current_threshold = 50
 
     _left_log_list = []
     _right_log_list = []
