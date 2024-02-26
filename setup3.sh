10d04c66522d16eb75f3d2c42e9e80f9c892949f
moram03 <mamora0312@gmail.com>
2024-02-26 20:04:42 +0000

    Updated Setup Files

--- setup3.sh
+++ setup3.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pip install -r requirements.txt
-pip list
+pip3 install -U pip setuptools wheel
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip3 install -r requirements.txt
+pip3 list
