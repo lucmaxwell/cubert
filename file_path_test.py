9c280737b6b114e528b0b2ffcfb2405dcc4b28f9
Andrew Stoyko <35904190+astoyko@users.noreply.github.com>
2024-03-05 16:24:56 -0600

    Update file_path_test.py

--- file_path_test.py
+++ file_path_test.py
@@ -1,7 +1,7 @@
 import os
 
 MODEL_NAME = "DQN_Tianshou_Vector.pth"
-model_path = './machine_leaning/' + MODEL_NAME
+model_path = '/home/pi/cubert/machine_learning/' + MODEL_NAME
 
 if os.path.exists(model_path):
     print('The file exists')
